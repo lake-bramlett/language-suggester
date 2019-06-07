4d03cc62b65c6eefd5c9cfa52ab38bd30e9858fe
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-07 10:19:26 -0700

    add user submit branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -75,7 +75,22 @@ function getMemory(memory) {
 function getMascot(mascot) {
   var mascot = $("input:radio[name=mascot]:checked").val();
   console.log('data gathered: mascot: ' + mascot);
-}
+
+  if (mascot === 'gopher') {
+    goTotal ++;
+  } else if (mascot === 'snake') {
+    pythonTotal ++;
+  } else if (mascot === 'bird') {
+    swiftTotal ++;
+  } else if (mascot === 'ruby') {
+    rubyTotal ++;
+  } else {
+    jsTotal ++;
+    cTotal ++;
+    rustTotal ++;
+  };
+};
+
 
 //user interface logic (jquery)
 $(document).ready(function() {
@@ -86,12 +101,37 @@ $('button.btn').click(function() {
   getAge();
   getCompany();
   getMemory();
+  getMascot();
 
   logTotals();
 
+  if (jsTotal > rustTotal && jsTotal > swiftTotal && jsTotal > goTotal && jsTotal > cTotal && jsTotal > pythonTotal && jsTotal > rubyTotal) {
+      console.log('javascript is suggested');
+      $('.col-results .results .javascript').show();
+    } else if (rustTotal > swiftTotal && rustTotal > goTotal && rustTotal > cTotal && rustTotal > pythonTotal && rustTotal > rubyTotal && rustTotal > jsTotal) {
+      console.log('rust is suggested');
+      $('.col-results .results .rust').show();
+    } else if (swiftTotal > goTotal && swiftTotal > cTotal && swiftTotal > pythonTotal && swiftTotal > rubyTotal && swiftTotal > jsTotal && swiftTotal > rustTotal) {
+      console.log('swift is suggested');
+      $('.col-results .results .swift').show();
+    } else if (goTotal > cTotal && goTotal > pythonTotal && goTotal > rubyTotal && goTotal > jsTotal && goTotal > rustTotal && goTotal > swiftTotal) {
+      console.log('go is suggested');
+      $('.col-results .results .golang').show();
+    } else if (cTotal > pythonTotal && cTotal > rubyTotal && cTotal > jsTotal && cTotal > rustTotal && cTotal > swiftTotal && cTotal > goTotal) {
+      console.log('c# is suggested');
+      $('.col-results .results .c-sharp').show();
+    } else if (pythonTotal > rubyTotal && pythonTotal > cTotal && pythonTotal > jsTotal && pythonTotal > goTotal && pythonTotal > swiftTotal && pythonTotal > rustTotal) {
+      console.log('python is suggested');
+      $('.col-results .results .python').show();
+    } else if (rubyTotal > jsTotal && rubyTotal > pythonTotal && rubyTotal > swiftTotal && rubyTotal > goTotal && rubyTotal > cTotal && rubyTotal > rustTotal) {
+      console.log('ruby is suggested');
+      $('.col-results .results .ruby').show();
+    } else {
+      console.log('multiple languages are suggested');
+      $('.col-results .results .ruby').show();
+    };
 
-})
 
 
-
+  });
 });
