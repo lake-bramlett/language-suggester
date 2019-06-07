78e9c82709ede3926692807035a66627984d0f09
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-07 11:08:07 -0700

    generate iframe based on results

--- js/scripts.js
+++ js/scripts.js
@@ -102,33 +102,41 @@ function getMascot(mascot) {
 
 function getResults() {
   var userName = $('#user-name').val();
-  $('.results .name').append(userName);
+  $('.results .name').text(userName);
   $('.results .copy').show();
 
   if (jsTotal > rustTotal && jsTotal > swiftTotal && jsTotal > goTotal && jsTotal > cTotal && jsTotal > pythonTotal && jsTotal > rubyTotal) {
       console.log('javascript is suggested');
       $('.col-results .results .javascript').show();
+      $('.col-results .results .jacascript div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/JavaScript"></iframe>');
     } else if (rustTotal > swiftTotal && rustTotal > goTotal && rustTotal > cTotal && rustTotal > pythonTotal && rustTotal > rubyTotal && rustTotal > jsTotal) {
       console.log('rust is suggested');
       $('.col-results .results .rust').show();
+      $('.col-results .results .rust div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/Rust_(programming_language)"></iframe>');
     } else if (swiftTotal > goTotal && swiftTotal > cTotal && swiftTotal > pythonTotal && swiftTotal > rubyTotal && swiftTotal > jsTotal && swiftTotal > rustTotal) {
       console.log('swift is suggested');
-      $('.col-results .results .swift').show();
+      $('.col-results .results .swift').show()
+      $('.col-results .results .swift div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/Swift_(programming_language)"></iframe>');
     } else if (goTotal > cTotal && goTotal > pythonTotal && goTotal > rubyTotal && goTotal > jsTotal && goTotal > rustTotal && goTotal > swiftTotal) {
       console.log('go is suggested');
       $('.col-results .results .golang').show();
+      $('.col-results .results .golang div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/Go_(programming_language)"></iframe>');
     } else if (cTotal > pythonTotal && cTotal > rubyTotal && cTotal > jsTotal && cTotal > rustTotal && cTotal > swiftTotal && cTotal > goTotal) {
       console.log('c# is suggested');
       $('.col-results .results .c-sharp').show();
+      $('.col-results .results .c-sharp div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"></iframe>');
     } else if (pythonTotal > rubyTotal && pythonTotal > cTotal && pythonTotal > jsTotal && pythonTotal > goTotal && pythonTotal > swiftTotal && pythonTotal > rustTotal) {
       console.log('python is suggested');
       $('.col-results .results .python').show();
+      $('.col-results .results .python div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/Python_(programming_language)"></iframe>');
     } else if (rubyTotal > jsTotal && rubyTotal > pythonTotal && rubyTotal > swiftTotal && rubyTotal > goTotal && rubyTotal > cTotal && rubyTotal > rustTotal) {
       console.log('ruby is suggested');
       $('.col-results .results .ruby').show();
+      $('.col-results .results .ruby div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/Ruby_(programming_language)"></iframe>');
     } else {
       console.log('multiple languages are suggested');
       $('.col-results .results .mutliple').show();
+      $('.col-results .results .multiple div.iframe').show().append().html('<iframe src="https://en.wikipedia.org/wiki/Programming_language"></iframe>');
     };
 
 }
